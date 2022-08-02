c0e39650f6d7f0d07d4d42a3c9dee10fd9a744c8
kazi331 <kazisharifulislam52@gmail.com>
2022-08-03 00:09:44 +0600

    title added

--- pages/all-products.jsx
+++ pages/all-products.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const allProducts = () => {
     return (
         <div>
-            <h2>All products</h2>
+            <h2 className='title'>All products</h2>
         </div>
     );
 };
