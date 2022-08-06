4ddbf7bcd946831b504ab54e0b5f8e0cd7937786
Md Shahriyar Alam <contact@shahriyar.dev>
2022-08-07 01:04:31 +0600

    Merge pagination branch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['fakestoreapi.com', 'i.postimg.cc', 'i.ibb.co'],
   },
